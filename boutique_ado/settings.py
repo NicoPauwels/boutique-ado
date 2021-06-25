267ae8e4fac93ff08616ae1aba3c3034255f7c9b
NicoPauwels <nico_pauwels@hotmail.com>
2021-06-25 15:27:33 +0000

    added product details functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -16,6 +16,8 @@ import os
 if os.path.exists("env.py"):
   import env
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
