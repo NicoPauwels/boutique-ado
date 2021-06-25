a6e769e7512b3d8dd15e87096b60d447d5428f06
NicoPauwels <nico_pauwels@hotmail.com>
2021-06-25 11:20:39 +0000

    hidden secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,15 +13,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists("env.py"):
+  import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@s66(f)6hw+vu67!=4%+hti+6ttp8g)d4=6i$$pyp!x8b!(ao'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
